b8a1da8b33e0a4df527a0fb143eac535b4e0ae4c
= <=>
2024-08-22 13:19:42 -0600

    update leaflet settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,6 @@ import { Layout } from "./components/layout/Layout"
 import { Contactanos } from "./Pages/Contactanos/Contactanos"
 import { Home } from "./Pages/Home/home"
 import { Producto } from "./Pages/Products/Producto"
-import L from 'leaflet';
-
-// Configura el mapa y marcadores usando L
-const map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
 
 function App() {
 
